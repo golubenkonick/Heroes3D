3b42ddbd291604f181e36d6516745224bcfd6cdd
BuildTools <unconfigured@null.spigotmc.org>
2023-01-04 12:01:10 +0200

    something3

--- Assets/MapScript.cs
+++ Assets/MapScript.cs
@@ -10,27 +10,35 @@ public class MapScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        cells = new GameObject[size][];
-        for (int i = 0;i < size; i++)
-        {
-            cells[i] = new GameObject[i+1];
-            for (int j = 0; j < i+1; j++)
-            {
-                float x = i + this.gameObject.transform.position.x;
-                float y = 0 + this.gameObject.transform.position.y;
-                float z = j + this.gameObject.transform.position.z;
-                Debug.Log($"{x} {y} {z}");
-                cells[i][j] = Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity, this.gameObject.transform) ;
-            }
-        }
+        //cells = new GameObject[size][];
+        //for (int i = 0;i < size; i++)
+        //{
+        //    cells[i] = new GameObject[i+1];
+        //    for (int j = 0; j < i+1; j++)
+        //    {
+        //        float x = i + this.gameObject.transform.position.x;
+        //        float y = 0 + this.gameObject.transform.position.y;
+        //        float z = j + this.gameObject.transform.position.z;
+        //        Debug.Log($"{x} {y} {z}");
+        //        cells[i][j] = Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity, this.gameObject.transform) ;
+        //    }
+        //}
+
+
+
+
+
+
+
+
     }
-    public float Distance(Vector2 cell1, Vector2 cell2)
-    {
-        float dx = Mathf.Abs(cell1.x - cell2.x);
-        float dy = Mathf.Abs(cell1.y - cell2.y);
-        float min = Mathf.Min(dx, dy); 
-        return min*Mathf.Sqrt(2) + Mathf.Abs(dx-dy);
-    }
+    //public float Distance(Vector2 cell1, Vector2 cell2)
+    //{
+    //    float dx = Mathf.Abs(cell1.x - cell2.x);
+    //    float dy = Mathf.Abs(cell1.y - cell2.y);
+    //    float min = Mathf.Min(dx, dy); 
+    //    return min*Mathf.Sqrt(2) + Mathf.Abs(dx-dy);
+    //}
 
     // Update is called once per frame
     void Update()
